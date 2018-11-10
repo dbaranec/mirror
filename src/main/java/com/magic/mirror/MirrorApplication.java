f615fed71ba2331083f02fe36a6eab57ea0d2448
Dominik Baranec <dominik.baranec@scr.sk>
2018-11-10 17:34:07 +0100

    init

---
+++ src/main/java/com/magic/mirror/MirrorApplication.java
@@ -0,0 +1,20 @@
+package com.magic.mirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MirrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirrorApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World! ";
+	}
+}
