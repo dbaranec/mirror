6a01681d7966634794e02ddfc9566d646a9a2532
Dominik Baranec <dominik.baranec@scr.sk>
2018-11-11 12:37:47 +0100

    [+] openfeign client

--- src/main/java/com/magic/mirror/MirrorApplication.java
+++ src/main/java/com/magic/mirror/MirrorApplication.java
@@ -1,13 +1,19 @@
 package com.magic.mirror;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableFeignClients
 public class MirrorApplication {
+
+	@Autowired
+	Book book;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MirrorApplication.class, args);
@@ -15,6 +21,9 @@ public class MirrorApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World! ";
+		return book.basic();
 	}
+
 }
+
+
